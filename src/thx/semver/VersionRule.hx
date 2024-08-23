cbc5c10369a1c11685bdd7e315a9f211a5bfbc76
numpaws <numpaws8@gmail.com>
2024-08-23 21:44:24 +1100

    trim fix

--- src/thx/semver/VersionRule.hx
+++ src/thx/semver/VersionRule.hx
@@ -1,27 +1,26 @@
 package thx.semver;
 
-using thx.semver.Version;
 using StringTools;
 
 abstract VersionRule(VersionComparator) from VersionComparator to VersionComparator {
   static var VERSION = ~/^(>=|<=|[v=><~^])?(\d+|[x*])(?:\.(\d+|[x*]))?(?:\.(\d+|[x*]))?(?:[-]([a-z0-9.-]+))?(?:[+]([a-z0-9.-]+))?$/i;
-  @:from public static function stringToVersionRule(s : String) : VersionRule {
-    var ors = s.split("||").map(function(comp) {
+
+  @:from
+  public static function stringToVersionRule(s:String):VersionRule {
+    var ors = s.split("||").map((comp) -> {
       comp = comp.trim();
       var p = comp.split(" - ");
-      return if(p.length == 1) {
+      return if (p.length == 1) {
         comp = comp.trim();
         p = (~/\s+/).split(comp);
-        if(p.length == 1) {
-          if(comp.length == 0) {
-            GreaterThanOrEqualVersion(Version.arrayToVersion([0,0,0]).withPre(VERSION.matched(5), VERSION.matched(6)));
-          } else if(!VERSION.match(comp)) {
-            throw 'invalid single pattern "$comp"';
-          } else {
+        if (p.length == 1) {
+          if (comp.length == 0) GreaterThanOrEqualVersion(Version.arrayToVersion([0,0,0]).withPre(VERSION.matched(5), VERSION.matched(6)));
+          else if (!VERSION.match(comp)) throw 'invalid single pattern "$comp"';
+          else {
             // one term pattern
-            var v:Array<Int> = versionArray(VERSION),
-                vf = v.concat([0, 0, 0]).slice(0, 3);
-            switch [VERSION.matched(1), v.length] {
+            var v = versionArray(VERSION);
+            var vf = v.concat([0, 0, 0]).slice(0, 3);
+            switch([VERSION.matched(1), v.length]) {
               case ["v", 0], ["=", 0], ["", 0], [null, 0]:
                 GreaterThanOrEqualVersion(Version.arrayToVersion(vf).withPre(VERSION.matched(5), VERSION.matched(6)));
               case ["v", 1], ["=", 1], ["", 1], [null, 1]:
@@ -79,89 +78,73 @@ abstract VersionRule(VersionComparator) from VersionComparator to VersionCompara
               case [p, _]: throw 'invalid prefix "$p" for rule $comp';
             };
           }
-        } else if(p.length == 2) {
-          if(!VERSION.match(p[0]))
-            throw 'left hand parameter is not a valid version rule "${p[0]}"';
-          var lp  = VERSION.matched(1),
-              lva = versionArray(VERSION),
-              lvf = lva.concat([0, 0, 0]).slice(0, 3),
-              lv  = Version.arrayToVersion(lvf).withPre(VERSION.matched(5), VERSION.matched(6));
+        } else if (p.length == 2) {
+          if (!VERSION.match(p[0])) throw 'left hand parameter is not a valid version rule "${p[0]}"';
+          var lp  = VERSION.matched(1);
+          var lva = versionArray(VERSION);
+          var lvf = lva.concat([0, 0, 0]).slice(0, 3);
+          var lv  = Version.arrayToVersion(lvf).withPre(VERSION.matched(5), VERSION.matched(6));
 
-          if(lp != ">" && lp != ">=")
-            throw 'invalid left parameter version prefix "${p[0]}", should be either > or >=';
-          if(!VERSION.match(p[1]))
-            throw 'left hand parameter is not a valid version rule "${p[0]}"';
-          var rp  = VERSION.matched(1),
-              rva = versionArray(VERSION),
-              rvf = rva.concat([0, 0, 0]).slice(0, 3),
-              rv  = Version.arrayToVersion(rvf).withPre(VERSION.matched(5), VERSION.matched(6));
-          if(rp != "<" && rp != "<=")
-            throw 'invalid right parameter version prefix "${p[1]}", should be either < or <=';
+          if (lp != ">" && lp != ">=") throw 'invalid left parameter version prefix "${p[0]}", should be either > or >=';
+          if (!VERSION.match(p[1])) throw 'left hand parameter is not a valid version rule "${p[0]}"';
+          var rp  = VERSION.matched(1);
+          var rva = versionArray(VERSION);
+          var rvf = rva.concat([0, 0, 0]).slice(0, 3);
+          var rv  = Version.arrayToVersion(rvf).withPre(VERSION.matched(5), VERSION.matched(6));
+          if (rp != "<" && rp != "<=") throw 'invalid right parameter version prefix "${p[1]}", should be either < or <=';
 
           AndRule(
             lp == ">" ? GreaterThanVersion(lv) : GreaterThanOrEqualVersion(lv),
             rp == "<" ? LessThanVersion(rv) : LessThanOrEqualVersion(rv)
           );
-        } else {
-          throw 'invalid multi pattern $comp';
-        }
+        } else throw 'invalid multi pattern $comp';
       } else if(p.length == 2) {
-        if(!VERSION.match(p[0]))
-            throw 'left range parameter is not a valid version rule "${p[0]}"';
-        if(VERSION.matched(1) != null && VERSION.matched(1) != "")
-            throw 'left range parameter should not be prefixed "${p[0]}"';
+        if (!VERSION.match(p[0])) throw 'left range parameter is not a valid version rule "${p[0]}"';
+        if (VERSION.matched(1) != null && VERSION.matched(1) != "") throw 'left range parameter should not be prefixed "${p[0]}"';
         var lv = Version.arrayToVersion(versionArray(VERSION).concat([0, 0, 0]).slice(0, 3)).withPre(VERSION.matched(5), VERSION.matched(6));
-        if(!VERSION.match(p[1]))
-            throw 'right range parameter is not a valid version rule "${p[1]}"';
-        if(VERSION.matched(1) != null && VERSION.matched(1) != "")
-            throw 'right range parameter should not be prefixed "${p[1]}"';
-        var rva = versionArray(VERSION),
-            rv = Version.arrayToVersion(rva.concat([0, 0, 0]).slice(0, 3)).withPre(VERSION.matched(5), VERSION.matched(6));
+        if (!VERSION.match(p[1])) throw 'right range parameter is not a valid version rule "${p[1]}"';
+        if (VERSION.matched(1) != null && VERSION.matched(1) != "") throw 'right range parameter should not be prefixed "${p[1]}"';
+        var rva = versionArray(VERSION);
+        var rv = Version.arrayToVersion(rva.concat([0, 0, 0]).slice(0, 3)).withPre(VERSION.matched(5), VERSION.matched(6));
 
-        if(rva.length == 1)
-          rv = rv.nextMajor();
-        else if(rva.length == 2)
-          rv = rv.nextMinor();
+        if (rva.length == 1) rv = rv.nextMajor();
+        else if (rva.length == 2) rv = rv.nextMinor();
 
         AndRule(
           GreaterThanOrEqualVersion(lv),
           rva.length == 3 ? LessThanOrEqualVersion(rv) : LessThanVersion(rv)
         );
-      } else {
-        throw 'invalid pattern "$comp"';
-      }
+      } else throw 'invalid pattern "$comp"';
     });
 
     var rule = null;
-    while(ors.length > 0) {
+    while (ors.length > 0) {
       var r = ors.pop();
-      if(null == rule)
-        rule = r;
-      else
-        rule = OrRule(r, rule);
+      if (null == rule) rule = r;
+      else rule = OrRule(r, rule);
     }
     return rule;
   }
 
   static var IS_DIGITS = ~/^\d+$/;
-  static function versionArray(re : EReg) {
+  static function versionArray(re:EReg) {
     var arr:Array<Int> = [];
     var t:String;
-    for(i in 2...5) {
+    for (i in 2...5) {
       t = re.matched(i);
-      if(null != t && IS_DIGITS.match(t))
-        arr.push(Std.parseInt(t));
-      else
-        break;
+      if (null != t && IS_DIGITS.match(t)) arr.push(Std.parseInt(t));
+      else break;
     }
     return arr;
   }
 
-  public static function versionRuleIsValid(rule : String)
+  public static function versionRuleIsValid(rule:String) {
     return try stringToVersionRule(rule) != null catch(e : Dynamic) false;
+  }
+    
 
-  public function isSatisfiedBy(version : Version) : Bool {
-    return switch this {
+  public function isSatisfiedBy(version:Version):Bool {
+    return switch(this) {
       case EqualVersion(ver):
         version == ver;
       case GreaterThanVersion(ver):
@@ -179,8 +162,9 @@ abstract VersionRule(VersionComparator) from VersionComparator to VersionCompara
     };
   }
 
-  @:to public function toString() : String
-    return switch ((this : VersionComparator)) {
+  @:to
+  public function toString():String
+    return switch(this) {
       case EqualVersion(ver):
         ver;
       case GreaterThanVersion(ver):
@@ -200,11 +184,11 @@ abstract VersionRule(VersionComparator) from VersionComparator to VersionCompara
 }
 
 enum VersionComparator {
-  EqualVersion(ver : Version);
-  GreaterThanVersion(ver : Version);
-  GreaterThanOrEqualVersion(ver : Version);
-  LessThanVersion(ver : Version);
-  LessThanOrEqualVersion(ver : Version);
-  AndRule(a : VersionComparator, b : VersionComparator);
-  OrRule(a : VersionComparator, b : VersionComparator);
+  EqualVersion(ver:Version);
+  GreaterThanVersion(ver:Version);
+  GreaterThanOrEqualVersion(ver:Version);
+  LessThanVersion(ver:Version);
+  LessThanOrEqualVersion(ver:Version);
+  AndRule(a:VersionComparator, b:VersionComparator);
+  OrRule(a:VersionComparator, b:VersionComparator);
 }
